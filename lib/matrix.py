0680cd501c518f1f86985035a0c9cf3073126676
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-07-02 21:40:23 +0900

    Commit numpy arr_sum and arr_multiply array

--- lib/matrix.py
+++ lib/matrix.py
@@ -4,3 +4,9 @@ class Matrix:
     def __init__(self, list):
         self.array = list
         self.shape = list.shape
+
+    def sum(self, num):
+        return self.array + num
+
+    def multiply(self, num):
+        return self.array * num
--- test/test_matrix.py
+++ test/test_matrix.py
@@ -17,5 +17,13 @@ class TestMatrix(unittest.TestCase):
     def test_slice(self):
         np.alltrue(np.array([[1, 2], [4, 5]]) == self.matrix.array[:2, 1:])
 
+    def test_arr_sum(self):
+        list = np.array([[9, 10, 11], [12, 13, 14], [15, 16, 17]])
+        np.alltrue(np.array([[9, 11, 13], [15, 17, 19], [21, 23, 25]]) == self.matrix.sum(list))
+
+    def test_arr_multiply(self):
+        list = np.array([[9, 10, 11], [12, 13, 14], [15, 16, 17]])
+        np.alltrue(np.array([[0, 10, 22], [36, 52, 70], [90, 112, 136]]) == self.matrix.multiply(list))
+
 if __name__ == "__main__":
     unittest.main()
