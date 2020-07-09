ae6ad63bc264c9078320ab7f2f4ca7f50a7e4a78
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-07-09 22:16:37 +0900

    Commit tokenier test

---
+++ dialogue_agent/test/test_tokenizer.py
@@ -0,0 +1,18 @@
+import unittest
+import sys
+sys.path.append("../lib")
+import tokenizer
+
+class TestTokenizer(unittest.TestCase):
+    def setUp(self):
+        self.text1 = "本を読んだ"
+        self.text2 = "本を読みました"
+
+    def test_tokenize1(self):
+        self.assertEqual(["本", "を", "読ん", "だ"], tokenizer.tokenize(self.text1))
+
+    def test_tokenize2(self):
+        self.assertEqual(["本", "を", "読み", "まし", "た"], tokenizer.tokenize(self.text2))
+
+if __name__ == "__main__":
+    unittest.main()
