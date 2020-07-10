29bec29c39caffb44b4dab16227dd2a3127c193d
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-07-10 17:38:19 +0900

    Update test_preprocess

--- dialogue_agent/test/test_preprocess.py
+++ dialogue_agent/test/test_preprocess.py
@@ -16,13 +16,13 @@ class TestDialogueAgent(unittest.TestCase):
     def test_raw_tokenize_text1(self):
         # FIXME: mecab-python3 does not provide a propper word devider, so even single-byte digits are counted as an element of list.
         # This probles seems not to be fixed yet: https://github.com/SamuraiT/mecab-python3/issues/19
-        self.assertEqual(["[", "初めて", "の", "TensorFlow", "]", "は", "定価", "2200円", "+", "税", "です"], self.preprocess.raw_tokenize(self.text1))
+        self.assertEqual(['[', '初めて', 'TensorFlow', ']', '定価', '2200円', '+', '税'], self.preprocess.raw_tokenize(self.text1))
 
     def test_raw_tokenize_text2(self):
-        self.assertEqual(["[", "初めて", "の", "\u3000", "TensorFlow", "]", "は", "定価", "２２００円", "+", "税", "です"], self.preprocess.raw_tokenize(self.text2))
+        self.assertEqual(['[', '初めて', 'TensorFlow', ']', '定価', '2200円', '+', '税'], self.preprocess.raw_tokenize(self.text2))
 
     def test_raw_tokenize_text3(self):
-        self.assertEqual(["[", "初めて", "の", "TensorFlow", "]", "は", "定価", "2200円", "+", "税", "です"], self.preprocess.raw_tokenize(self.text3))
+        self.assertEqual(['[', '初めて', 'TensorFlow', ']', '定価', '2200円', '+', '税'], self.preprocess.raw_tokenize(self.text3))
 
     def test_neologdn_normalized_token_text2(self):
         self.assertEqual(self.preprocess.raw_tokenize(self.text1), self.preprocess.neologdn_normalized_token(self.text2))
@@ -35,7 +35,7 @@ class TestDialogueAgent(unittest.TestCase):
        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]) == self.preprocess.bow(self.texts))
 
     def test_vocaburaly(self):
-        self.assertEqual({'(株)': 0, 'し': 1, 'た': 2, 'に': 3, '入社': 4, '研究': 5, '自然言語処理': 6}, self.preprocess.vocabulary(self.texts))
+        self.assertEqual({'(株)': 0, '自然言語処理': 4, '研究': 3, '入社': 2, 'する': 1}, self.preprocess.vocabulary(self.texts))
 
 if __name__ == "__main__":
     unittest.main()
