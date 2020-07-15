01a4353fe9f1ceceeaf4e8d5f756b78b234ae229
oasist <bin.lhkty,gtthj.o.zs@gmail.com>
2020-07-15 23:44:04 +0900

    Commit feature_union

---
+++ dialogue_agent/lib/feature_union.py
@@ -0,0 +1,24 @@
+import scipy
+from sklearn.feature_extraction.text import CountVectorizer
+from tokenizer import tokenize
+
+class FeatureUnion:
+    def __init__(self, texts, ngram_range):
+        # Word-based BoW
+        self.word_bow_vectorizer = CountVectorizer(tokenizer=tokenize)
+        self.word_bow_vectorizer.fit(texts)
+        self.word_bow = self.word_bow_vectorizer.transform(texts)
+        # bi-gram BoW
+        self.char_bigram_vectorizer = CountVectorizer(analyzer="char", ngram_range=ngram_range)
+        self.char_bigram_vectorizer.fit(texts)
+        self.char_bigram_bow = self.char_bigram_vectorizer.transform(texts)
+
+    def word_bow_array(self):
+        return self.word_bow.toarray()
+
+    def char_bigram_bow_array(self):
+        return self.char_bigram_bow.toarray()
+
+    def unite_feature(self):
+        feature = scipy.sparse.hstack((self.word_bow, self.char_bigram_bow))
+        return feature.toarray()
---
+++ dialogue_agent/test/test_feature_union.py
@@ -0,0 +1,46 @@
+import unittest
+import sys
+sys.path.append("../lib")
+sys.path.append("../lib/concern")
+from feature_union import FeatureUnion
+import numpy as np
+from numpy.testing import assert_array_equal
+
+class TestFeatureUnion(unittest.TestCase):
+    def setUp(self):
+        texts = [
+            "私は私のことが好きなあなたを愛しています",
+            "私はラーメンが好きです",
+            "富士山は日本一高い山です",
+        ]
+        self.feature_union = FeatureUnion(texts, (2, 2))
+
+    def test_word_bow(self):
+        assert_array_equal(
+            np.array(
+                [
+                    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 2, 0],
+                    [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1]
+                ]
+            ), self.feature_union.word_bow_array()
+        )
+
+    def test_bigram_bow(self):
+        assert_array_equal(
+            np.array(
+                [
+                    [1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1]
+                ]
+            ), self.feature_union.char_bigram_bow_array()
+        )
+
+    def test_unite_feature(self):
+        word_bow = self.feature_union.word_bow_array()
+        char_bigram_bow = self.feature_union.char_bigram_bow_array()
+        assert_array_equal(np.hstack((word_bow, char_bigram_bow)), self.feature_union.unite_feature())
+
+if __name__ == "__main__":
+    unittest.main()
