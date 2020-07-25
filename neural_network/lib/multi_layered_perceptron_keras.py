ff67dd0c504e54712a3e5005ecef8fc518e604e2
Oasist <bin.lhkty,gtthj.o.zs@gmail.com>
2020-07-25 22:16:03 +0900

    Define fit_model method in multi_layered_perceptron_keras

--- neural_network/lib/multi_layered_perceptron_keras.py
+++ neural_network/lib/multi_layered_perceptron_keras.py
@@ -1,5 +1,8 @@
 from keras.layers import Dense
 from keras.models import Sequential
+import sys
+sys.path.append("../lib/concern")
+from n_dim_generator import generate_n_dim
 
 class MultiLayeredPerceptronKeras:
     def __init__(self):
@@ -13,5 +16,13 @@ class MultiLayeredPerceptronKeras:
         self.model.add(Dense(units=1, activation="sigmoid"))
         self.model.compile(loss="binary_crossentropy", optimizer="adam")
 
-    def get_model_weights(self):
-        return self.model.layers[0].get_weights()
+    def fit_model(self):
+        X = np.array([
+            generate_n_dim(3),
+            generate_n_dim(3),
+        ])
+        y = np.array([
+            0,
+            1
+        ])
+        self.model.fit(X, y, batch_size=32, epochs=100)
