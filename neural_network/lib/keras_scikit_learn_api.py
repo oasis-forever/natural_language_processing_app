f593545eeea0ae4da1fbd80b13f852877ae3ddc2
Oasist <bin.lhkty,gtthj.o.zs@gmail.com>
2020-07-27 16:50:58 +0900

    WIP: Commit keras_scikit_learn_api

---
+++ neural_network/lib/keras_scikit_learn_api.py
@@ -0,0 +1,26 @@
+import numpy as np
+from keras.wrappers.scikit_learn import KerasClassifier
+import numpy as np
+from keras.utils import to_categorical
+import sys
+sys.path.append("../lib/concern")
+from n_dim_generator import generate_n_dim
+from mlp_builder import build_multi_layered_perceptron
+
+class KerasScikitLearnApi:
+    def __init__(self):
+        pass
+
+    def fit_classifier(self):
+        X = np.array([
+            generate_n_dim(100),
+            generate_n_dim(100),
+        ])
+        y = np.array([0, 1])
+        input_dim = X.shape[1]
+        n_labels = max(y) + 1
+        self.classifier = KerasClassifier(build_fn=build_multi_layered_perceptron, input_dim=input_dim, hidden_units=32, output_dim=n_labels)
+        self.classifier.fit(X, y, epochs=100)
+
+    def predict(self, some_feature):
+        return self.classifier.predict(some_feature)
---
+++ neural_network/test/test_keras_scikit_learn_api.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+sys.path.append("../lib")
+sys.path.append("../lib/concern")
+from keras_scikit_learn_api import KerasScikitLearnApi
+
+class TestDialogueAgent(unittest.TestCase):
+    def setUp(self):
+        self.ksla = KerasScikitLearnApi()
+        self.ksla.fit_classifier()
+
+    def test_reply(self):
+        self.assertEqual([1], self.ksla.predict(some_feature))
+
+if __name__ == "__main__":
+    unittest.main()
