f0d94807fd10b1e2d8e4da5db25fb403360738ae
Oasist <bin.lhkty,gtthj.o.zs@gmail.com>
2020-07-28 23:46:55 +0900

    Commit build_four_layered_perceptron

--- neural_network/lib/concern/mlp_builder.py
+++ neural_network/lib/concern/mlp_builder.py
@@ -7,3 +7,12 @@ def build_two_layered_perceptron(hidden_units=32, input_dim=100, output_dim=10, 
     mlp.add(Dense(units=output_dim, activation="softmax"))
     mlp.compile(loss=loss, optimizer="adam")
     return mlp
+
+def build_four_layered_perceptron(hidden_units=32, input_dim=100, output_dim=10, loss="categorical_crossentropy"):
+    mlp = Sequential()
+    mlp.add(Dense(units=hidden_units, input_dim=input_dim, activation="relu"))
+    mlp.add(Dense(units=hidden_units, input_dim=input_dim, activation="relu"))
+    mlp.add(Dense(units=hidden_units, input_dim=input_dim, activation="relu"))
+    mlp.add(Dense(units=output_dim, activation="softmax"))
+    mlp.compile(loss=loss, optimizer="adam")
+    return mlp
