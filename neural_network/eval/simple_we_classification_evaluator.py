5625312e1f0bbabc9b0450127f3d4b7c00349f94
Oasist <bin.lhkty,gtthj.o.zs@gmail.com>
2020-08-04 00:53:57 +0900

    Refactor simple_we_classification_evaluator

--- neural_network/eval/simple_we_classification_evaluator.py
+++ neural_network/eval/simple_we_classification_evaluator.py
@@ -9,18 +9,30 @@ from simple_we_classification import SimpleWeClassification
 import numpy as np
 import pandas as pd
 
+class SimpleWeClassificationEvaluator:
+    def __init__(self):
+        self.swc = SimpleWeClassification()
+
+    def prepare_data(self, csv_data):
+        date = pd.read_csv(csv_data)
+        X_data = np.array([self.swc.calc_text_feature(text) for text in date["text"]])
+        y_data = np.array(date["label"])
+        return X_data, y_data
+
+    def train(self, X_train, y_train):
+        self.svc = SVC()
+        self.svc.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        self.y_pred = self.svc.predict(X_test)
+
+    def evaluate(self, y_test):
+        print(accuracy_score(y_test, self.y_pred))
+
 if __name__ == "__main__":
-    swc = SimpleWeClassification()
-    training_date = pd.read_csv("../csv/training_data.csv")
-    test_date = pd.read_csv("../csv/test_data.csv")
-
-    X_train = np.array([swc.calc_text_feature(text) for text in training_date["text"]])
-    y_train = np.array(training_date["label"])
-
-    X_test = np.array([swc.calc_text_feature(text) for text in test_date["text"]])
-    y_test = np.array(test_date["label"])
-
-    svc = SVC()
-    svc.fit(X_train, y_train)
-    y_pred = svc.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
+    swce = SimpleWeClassificationEvaluator()
+    X_train, y_train = swce.prepare_data("../csv/training_data.csv")
+    swce.train(X_train, y_train)
+    X_test, y_test = swce.prepare_data("../csv/test_data.csv")
+    swce.predict(X_test)
+    swce.evaluate(y_test)
